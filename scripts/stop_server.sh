2ff32c6d407a97c99409ffac24cd842239a541a6
MADHANPRASATH K <160216345+MADHANKATHIRVEL@users.noreply.github.com>
2024-06-07 16:48:06 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,7 +3,7 @@
 echo "Stopping the server"
 
 # Attempt to kill the server process and capture the status
-if pkill -f serve; then
+if pkill -f "serve -s /var/www/html"; then
   echo "Server stopped successfully."
 else
   echo "No matching server process found or failed to stop the server."
