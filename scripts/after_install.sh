3da27b92c1dcb1a0c052faf522a1c7b0facbbd20
MADHANPRASATH K <160216345+MADHANKATHIRVEL@users.noreply.github.com>
2024-06-08 15:33:15 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Install Node.js dependencies
+# Install Node.js dependencies for creation of nodejs
 echo "AfterInstall phase: Installing Node.js dependencies..."
 cd /var/www/html
 npm install
