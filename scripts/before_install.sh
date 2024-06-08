aaeecaca803351a2212daa6e75931c4ec8c6a4ef
MADHANPRASATH K <160216345+MADHANKATHIRVEL@users.noreply.github.com>
2024-06-08 15:45:57 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-# BeforeInstall script to handle existing files
+# BeforeInstall script to handle existing files and install Node.js and npm
 echo "BeforeInstall phase: Removing existing files to avoid conflicts..."
 rm -rf /var/www/html/*
+
+# Install Node.js and npm if not installed
+echo "BeforeInstall phase: Installing Node.js and npm if not installed..."
+if ! command -v node &> /dev/null
+then
+    curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+    sudo yum install -y nodejs
+fi
